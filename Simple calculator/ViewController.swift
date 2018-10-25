bad4bb87d20f6d6bf381c251419715e0378993b5
20171105113 <wy1239072462@163.com>
2018-10-25 10:06:08 +0800

    2.0
    增加了白天夜间模式切换功能，太阳标志和月亮标志也会随之变化。

---
+++ Simple calculator/ViewController.swift
@@ -0,0 +1,152 @@
+//
+//  ViewController.swift
+//  Simple calculator
+//
+//  Created by 20171105113 on 2018/9/27.
+//  Copyright © 2018年 20171105113. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var temp:Double = 0
+    var sum:Double = 0
+    var sum2:Double=0
+    var sum3:String = ""
+    var flag = 0
+    var t1:Bool = false
+    var isBlack:Bool = false
+    var t2:Bool = false
+    @IBAction func Change2(_ sender: Any) {
+        if(isBlack){
+            self.view.backgroundColor = UIColor.white
+            isBlack = false
+            t1 = true
+            t2 = false
+            
+        }
+        else{
+            self.view.backgroundColor = UIColor.black
+            isBlack = true
+            t1 = false
+            t2 = true
+        }
+        if(t2)
+        {
+            self.sun1.isHidden = true
+        }
+        else{
+            self.sun1.isHidden = false
+        }
+        if(t1)
+        {
+            self.moon1.isHidden = true
+        }
+        else{
+            self.moon1.isHidden = false
+        }
+    }
+    @IBOutlet weak var Display: UITextField!
+    var isYellow:Bool = false
+    @IBAction func Changeb(_ sender: Any) {
+        if(isYellow){
+            self.view.backgroundColor = UIColor.white
+            isYellow = false
+        }else{
+            self.view.backgroundColor = UIColor.yellow
+            isYellow = true
+        }
+    }
+    @IBAction func Buttono(_ sender: Any) {
+        Display.text = Display.text!+"."
+    }
+    @IBAction func Buttontest1(_ sender: Any) {
+        Display.text = Display.text!+"0"
+    }
+    
+    @IBAction func Button1(_ sender: Any) {
+        Display.text = Display.text!+"1"
+    }
+    
+    @IBAction func Button2(_ sender: Any) {
+        Display.text = Display.text!+"2"
+    }
+    
+    @IBAction func Button3(_ sender: Any) {
+        Display.text = Display.text!+"3"
+    }
+    
+    @IBAction func Button4(_ sender: Any) {
+        Display.text = Display.text!+"4"
+    }
+    
+    @IBAction func Button5(_ sender: Any) {
+        Display.text = Display.text!+"5"
+    }
+    
+    @IBAction func Button61(_ sender: Any) {
+        Display.text = Display.text!+"6"
+    }
+    @IBAction func Button7(_ sender: Any) {
+        Display.text = Display.text!+"7"
+    }
+    
+    @IBAction func Button8(_ sender: Any) {
+        Display.text = Display.text!+"8"
+    }
+    
+    @IBAction func Button9(_ sender: Any) {
+        Display.text = Display.text!+"9"
+    }
+    
+    @IBAction func Buutonadd(_ sender: Any) {
+        temp = Double(Display.text!)!
+        Display.text = ""
+        flag = 1
+    }
+    
+    @IBAction func Buttonreduce(_ sender: Any) {
+        temp = Double(Display.text!)!
+        Display.text = ""
+        flag = 2
+    }
+    
+    @IBAction func Buttonride(_ sender: Any) {
+        temp = Double(Display.text!)!
+        Display.text = ""
+        flag = 3
+    }
+    
+    @IBAction func Buttonexecpt(_ sender: Any) {
+        temp = Double(Display.text!)!
+        Display.text = ""
+        flag = 4
+    }
+    
+    @IBAction func Buttoncalculate(_ sender: Any) {
+        if flag == 1{
+        sum = temp + Double(Display.text!)!
+        }
+        else if flag == 2 {
+        sum = temp - Double(Display.text!)!
+        }
+        else if flag == 3{
+        sum = temp * Double(Display.text!)!
+        }
+        else if flag == 4{
+        sum = temp / Double(Display.text!)!
+        }
+        let sum3 = String(format: "%.2f", Double(sum))
+        _ = Double(sum3)
+        Display.text = "\(sum3)"
+    }
+    
+    @IBAction func Clear(_ sender: Any) {
+        Display.text = ""
+    }
+
+    @IBOutlet weak var sun1: UILabel!
+    @IBOutlet weak var moon1: UILabel!
+    
+    @IBOutlet weak var pic: UIImageView!
+}
---
+++ Simple calculatorTests/Simple_calculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Simple_calculatorTests.swift
+//  Simple calculatorTests
+//
+//  Created by 20171105113 on 2018/9/27.
+//  Copyright © 2018年 20171105113. All rights reserved.
+//
+
+import XCTest
+@testable import Simple_calculator
+
+class Simple_calculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
